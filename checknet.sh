a47dc115d0ac5943fe3b83c9df7cac204bf2b20b
xigua <9163712+HUI2125281@users.noreply.github.com>
2022-02-12 11:54:25 +0800

    Update checknet.sh
    #延迟30秒，避免拨号没有成功之前，造成反复重启。

--- checknet.sh
+++ checknet.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+#延迟30秒，避免拨号没有成功之前，造成反复重启。
+sleep 30
 nc -z -w 3 180.101.49.11 80 || nc -z -w 3 180.163.122.229 80
-if [ $? -eq 0 ]
-then
-  echo -e $(date) "The script ran ok" >> /var/log/checknet.log
-  exit 0
+if [ $? -eq 0 ]; then
+  echo -e $(date) "The script ran ok" >> /vmfs/volumes/datastore1/log/checknet.log
+  #exit 0
 else
-  echo -e $(date) "The script failed" >> /var/log/checknet.log
+  echo -e $(date) "The script failed" >> /vmfs/volumes/datastore1/log/checknet.log
   vim-cmd vmsvc/power.reset 2
-  exit 1
+  #exit 1
 fi
